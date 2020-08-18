ece9bdf0a259b0c11ec52829b40e554298fdf38f
Luckycarrot42 <eicarriemontecinos@gmail.com>
2020-08-17 22:57:34 -0400

    input para  filtrar productos

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p v-text="busy"></p>
+    <p v-text="sale_record"></p>
+    <input type="text" placeholder="buscar" v-model="search" />
+    <ul v-if="searchById">
+      <li>{{ searchById.nombre }}</li>
+    </ul>
+    <ul>
+      <li v-for="(product, index) in availableProducts" :key="index">{{ product.nombre }}</li>
+    </ul>
+    <h3>Ofertas de último minuto</h3>
+    <ul>
+      <li v-for="(product, index) in cheapProducts" :key="index">{{ product.nombre }} - {{ product.precio }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {mapState, mapGetters} from "vuex";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data (){
+    return {
+      search: null
+    }
+  },
+  computed: {
+    ...mapState (["isBusy", "sales"]),  //helper de vuex, importarlo para acceder a var de state mas facilmente
+    ...mapGetters (["availableProducts", "cheapProducts", "getProductById"]), //agregar getters in store.js para acarrear informacion
+    busy(){
+      let statusBusy = this.isBusy ? 'Ocupado' : 'Disponible'
+      return `Estado ${statusBusy}`
+    },
+    sale_record(){
+      return `Total de ventas es ${this.sales}`
+      // $ interno refiere a plugin
+    },
+    searchById (){
+      return this.getProductById(this.search)
+    }
   }
+  
 }
 </script>
 
@@ -24,5 +54,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ul{
+  list-style-type: none;
 }
 </style>
